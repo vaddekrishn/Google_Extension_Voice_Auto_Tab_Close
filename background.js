77a49e1ba47a66e248d9309d7c7cecc776780d66
vaddekrishn <krishnaxkv@gmail.com>
2024-05-31 14:10:14 -0600

    Save this as the test for closing the extra tab changed only background.js in this 2nd push

--- background.js
+++ background.js
@@ -1,11 +1,18 @@
+let existingTabId = null;
+
 chrome.tabs.onCreated.addListener(function(tab) {
   if (tab.url.startsWith("https://voice.google.com/")) {
-    chrome.tabs.query({ url: "https://voice.google.com/*" }, function(tabs) {
-      if (tabs.length > 1) {
-        chrome.tabs.remove(tabs[0].id, function() {
-          console.log("Closed the first created tab with URL starting with https://voice.google.com/");
-        });
-      }
-    });
+    if (existingTabId !== null) {
+      chrome.tabs.remove(existingTabId, function() {
+        console.log("Closed the old tab with URL starting with https://voice.google.com/");
+      });
+    }
+    existingTabId = tab.id;
+  }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId) {
+  if (tabId === existingTabId) {
+    existingTabId = null;
   }
 });
